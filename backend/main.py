373c105ff44b04d6d6a3c9e4bc21ee03ca7c5c8d
Emily Ho <icianho@gmail.com>
2024-01-20 12:30:33 -0800

    before we lose it

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,25 @@
 from flask import Flask, jsonify, request
+from google.cloud import storage
 
 app = Flask(__name__)
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
 
 # Use the application default credentials.
 cred = credentials.ApplicationDefault()
 
+# initialize firebase app
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+# get bucket name from env vars
+bucket_name = os.environ.get('BUCKET_NAME')
+if not bucket_name:
+    print("ERROR: BUCKET NAME NOT DEFINED")
+    exit(1)
 
 #######################################3 THIS IS THE CODE FOR UPLOADING DATA TO FIREBASE
 # doc_ref = db.collection("events").document("2")
@@ -28,6 +37,31 @@ db = firestore.client()
 #     )
 # print(res)
 
+def upload_bytes_to_gcs (bucket, blob, bytes_to_upload):
+    '''
+    Helper function for uploading bytes (image file bytes for this project) to Google Cloud Storage
+    '''
+
+    # Initialize a client
+    client = storage.Client()
+
+    # Get the bucket
+    bucket = client.get_bucket(bucket_name)
+
+    # Create a blob (object) in the bucket
+    blob = bucket.blob(blob)
+
+    # Upload the bytes to the blob
+    res = blob.upload_from_string(bytes_to_upload, content_type="image/png")
+
+    print(f"response is {res}")
+    return res
+
+def create_event_to_firebase (collection_name, document_name, json_to_dump):
+    doc_ref = db.collection(collection_name).document(document_name)
+    res = doc_ref.set(json_to_dump)
+    print(res)
+
 @app.route('/')
 def hello_world():
     return jsonify(message='Hello, World!')
@@ -40,29 +74,40 @@ def all_events_get():
 def upload_event_json_post(uuid):
     try:
         data = request.get_json(force=True)
-        columns = ['club_or_affiliation', 'eventname', 'date', 'time', 'flyer', 'social_link', 'location']
         
         # if tags is not in json, add 1
         if 'tags' not in data:
             data['tags'] = []
 
-        # add the rest of the columsn if they don't exist yet
+        # add the rest of the columns if they don't exist yet
         columns = ['club_or_affiliation', 'eventname', 'date', 'time', 'flyer', 'social_link', 'location']
         for col in columns:
             if col not in data:
-                data['col'] = None
+                data[col] = None
+
+        # assemble flyer link
+        data['flyer'] = 'gs://'
 
         # TODO upload to firebase (uuid as the document)
+        create_event_to_firebase('events', uuid, data)
 
         # return at the very least the uuid (see if that's posisble to get)
-        return {'data': data}, 200
+        return data, 200
     except Exception as e:
         print(f"Exception in /UploadEventJson: {e}")
         return {"exception": str(e)}, 400
     
 @app.route('/UploadImage/<uuid>', methods=['POST'])
 def upload_event_image_post(uuid):
-    print(request.files)
+    # get image bytes
+    bytes_to_send = request.data
+
+    # assemble blob name
+    blob_name = f'{uuid}.png'
+
+    # upload file to GCS
+    upload_bytes_to_gcs(bucket_name, blob_name, bytes_to_send)
+
     return {}, 200
 
 if __name__ == '__main__':
--- backend/test.py
+++ backend/test.py
@@ -8,12 +8,23 @@ def test_upload_event():
     with open('graph.png', 'rb') as infile:
         # upload the json
         req_url = f'{url}/UploadEventJson/{event_uuid}'
-        res = requests.post(req_url, json={'test': 'hi'})
+        req_json = {
+            "club_or_affiliation": "TSA",
+            "eventname": "Boba making",
+            "date": "1/19/2024",
+            "time": "15:00",
+            "flyer": "gs://cruzhack-24.appspot.com/graph.png",
+            "social_link": "insta/discord",
+            "location": "C9 Community Room",
+            "tags": ["tag 1", "tag2"]
+        }
+        res = requests.post(req_url, json=req_json)
         print(res.text)
 
         # upload the image
         req_url = f'{url}/UploadImage/{event_uuid}'
-        res = requests.post(req_url, files={'file': infile})
+        # res = requests.post(req_url, files={'file': infile})
+        res = requests.post(req_url, data=infile)
         print(res.text)
 
 test_upload_event()
